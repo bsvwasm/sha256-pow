2e2cea87805f0cd32fc84a18dcfaa26192bc570a
Nick Carton <nick.carton@protonmail.com>
2021-12-28 00:40:22 +1100

    Fix deno example

--- examples/web/main.ts
+++ examples/web/main.ts
@@ -1,16 +1,13 @@
-import init, { mine, verify } from '../../pkg/web/sha256_pow.js';
+import init, { mine, verify } from 'https://deno.land/x/sha256pow@0.1.0/sha256_pow.js';
+import { encodeToString } from 'https://deno.land/std@0.53.0/encoding/hex.ts';
 
 await init();
 
-const target = "00000021e800000000000000000000000000000000000000000000000000000";
+const target = "0021e8000000000000000000000000000000000000000000000000000000000";
 
 const powBuffer = new TextEncoder().encode("Hello, PoW!");
 
-window.addEventListener("pow-event", (hex) => {
-    console.log(hex)
-});
-
 const pow = await mine(powBuffer, target);
 
-console.log("Can verify PoW?", verify(pow, target));
+console.log("Can verify PoW?", verify(pow, target), "nonce", pow.get_nonce(), "hash", encodeToString(pow.get_hash()));
 
